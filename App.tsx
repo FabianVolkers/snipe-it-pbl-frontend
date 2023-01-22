1128c75ab4ccd2d03ee3419119615190433b74ae
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-01-22 11:26:17 +0000

    Setup react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './app/views/Home'
+import ScanScreen from './app/views/CameraView'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: 'Awesome app' }}
+          />
+     <Stack.Screen
+            name="Scan"
+            component={ScanScreen}
+          />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+/*   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  ); */
 }
 
 const styles = StyleSheet.create({
