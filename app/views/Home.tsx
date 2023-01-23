71870b8b66c4be47f748ba1b329c108a4d81854c
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-01-23 10:17:20 +0000

    Add navigation button to asset 1

--- app/views/Home.tsx
+++ app/views/Home.tsx
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import styles from '../Styles'
+import { StatusBar } from "expo-status-bar";
+import { Button, Text, View } from "react-native";
+import styles from "../Styles";
 
-const Home = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      );
-}
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title={"Asset 1"}
+        onPress={() => navigation.navigate("SearchStack", {
+          screen: "Asset",
+          params: { id: "1" },
+        })}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
-export default Home
+export default Home;
