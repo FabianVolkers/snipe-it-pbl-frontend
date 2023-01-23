b369fb8d85a1d2000da83acacbcca14d68f2475d
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-01-23 10:16:25 +0000

    Setup AssetScreen

--- app/views/AssetView.tsx
+++ app/views/AssetView.tsx
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import styles from '../Styles'
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { useQuery } from "react-query";
+import { getAsset } from "../snipe-it/api";
+import styles from "../Styles";
 
-const AssetScreen = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      );
-}
+const AssetScreen = ({ navigation, route }) => {
+  //const { id } = useParams<{ id: string }>();
+  const [asset, setAsset] = useState({});
+  const id = route.params.id;
 
-export default AssetScreen
+  useEffect(() => {
+
+    getData();
+    /* const { data, status } = useQuery(`asset-${id}`, (id) => getAsset, {
+      staleTime: 60 * 10000000, // 10000 minute = around 1 week
+      cacheTime: 60 * 10000000,
+    }); */
+  }, [])
+  const getData = async () => {
+    const data = await getAsset(id)
+    console.log(data)
+    setAsset(data)
+    console.log(asset)
+  } 
+
+
+  return (
+    <View style={styles.container}>
+    
+      <Text>Asset {asset['asset_tag']}</Text>
+      <Button title={'Light it up'}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default AssetScreen;
